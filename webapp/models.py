b1df07de4ad5eec8f618131f70df1afe3de0623c
SalaSander <bridtssander@gmail.com>
2016-04-19 13:50:56 +0200

    database migrated

--- webapp/models.py
+++ webapp/models.py
@@ -1,17 +1,49 @@
 from django.db import models
 
+
+class PandType(models.Model):
+    pandtype = models.CharField(max_length=128) #kantoor,huis, appartement, ...
+
+    def __str__(self):
+        return self.id
+
+
+class Handelstatus(models.Model):
+    status = models.CharField(max_length=128) #verkoop, verhuur, verpachten, ...
+
+    def __str__(self):
+        return self.id
+
+
+class Voortgang(models.Model):
+    status = models.CharField(max_length=128) #online, optie, bij notaris, ...
+    def __str__(self):
+        return self.id
+
+class Toegangslevel(models.Model):
+    toegangslevelnaam = models.CharField(max_length=255)
+    def __str__(self):
+        return self.id
+
+
+class Stad(models.Model):
+    postcode = models.SmallIntegerField
+    stadsnaam = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.id
+
 class Gebruiker(models.Model):
     #Id implemented by django
-    #userid = models.Autofield(primary_key=True)
-    voornaam = models.charField(max_length=128)
-    naam =  models.charField(max_length=128)
-    email = models.charField(max_length=128)
-    straatnaam = models.charField(max_length=128)
+    voornaam = models.CharField(max_length=128)
+    naam =  models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    straatnaam = models.CharField(max_length=128)
     huisnr = models.IntegerField()
     postcode = models.IntegerField()
-    busnr = models.charField(max_length=10)
+    busnr = models.CharField(max_length=10)
     telefoonnr = models.IntegerField()
-    password= models.charField(max_length=30) #ToDo: add hashes + saltes (import)
+    password= models.CharField(max_length=30) #ToDo: add hashes + saltes (import)
     toegangslevel = models.ForeignKey(Toegangslevel)
 
     def __str__(self):
@@ -21,70 +53,41 @@ class Gebruiker(models.Model):
 class Log(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     gebruiker = models.ForeignKey(Gebruiker)
-    logText = models.charField(255)
+    logText = models.CharField(max_length=255)
     
     def __str__(self):
         return self.id
 
-class Toegangslevel(models.Model):
-    toegangslevelnaam = models.charField(255)
-    
-    def __str__(self):
-        return self.id
+
 
 class Pand(models.Model):
-	#id autocreated by django
+    #id autocreated by django
     gebruiker = models.ForeignKey(Gebruiker)
-	straatnaam = models.CharField(max_length=128)
-	huisnr = models.SmallIntegerField
-	postcodeID = models.ForeignKey(Steden)
-	pandtype = models.ForeignKey(Pandtype)
-	handelstatus = models.ForeignKey(Handelstatus)
-	voortgang = models.ForeignKey(Voortgang)
+    straatnaam = models.CharField(max_length=128)
+    huisnr = models.SmallIntegerField
+    postcodeID = models.ForeignKey(Stad)
+    pandtype = models.ForeignKey(PandType)
+    handelstatus = models.ForeignKey(Handelstatus)
+    voortgang = models.ForeignKey(Voortgang)
 
-	def __str__(self):
+    def __str__(self):
         return self.id
 
 
 class Tag(models.Model):
     #id autocreated by django
     tagnaam = models.CharField(max_length=128)
-    Pand = models.ManyToManyField(Panden)
+    Pand = models.ManyToManyField(Pand)
 
-	def __str__(self):
+    def __str__(self):
         return self.id
 
 
-class Stad(models.Model):
-	postcode = models.SmallIntegerField
-	stadsnaam = models.CharField(max_length=128)
-
-	def __str__(self):
-        return self.id
 
 class Foto(models.Model):
-	url = models.CharField(max_length=256)
-	pand = models.ManyToManyField(Panden)
+    url = models.CharField(max_length=255)
+    pand = models.ManyToManyField(Pand)
 
-	def __str__(self):
+    def __str__(self):
         return self.id
 
-
-class PandType(model.Model):
-	pandtype = models.CharField(max_length=128) #kantoor,huis, appartement, ...
-
-	def __str__(self):
-        return self.id
-
-
-class Handelstatus(models.Model)
-	status = models.CharField(max_length=128) #verkoop, verhuur, verpachten, ...
-
-	def __str__(self):
-        return self.id
-
-
-class Voortgang(models.Model)
-	status = models.Charfiels(max_length=128) #online, optie, bij notaris, ...
-	def __str__(self):
-        return self.id
