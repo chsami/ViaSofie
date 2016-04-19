d2c5f7a00a4ac862639ba2e940351b140527ae73
Sander Bridts <bridtssander@gmail.com>
2016-04-19 10:50:38 +0200

    bugfix

--- webapp/views.py
+++ webapp/views.py
@@ -4,17 +4,24 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return render(request, 'webapp/index.html')
-	def about(request):
+
+def about(request):
 	return render(request, 'webapp/about.html')
-	def contact(request):
+
+def contact(request):
 	return render(request, 'webapp/contact.html')
-	def advies(request):
+
+def advies(request):
 	return render(request, 'webapp/advies.html')
-	def huren(request):
+
+def huren(request):
 	return render(request, 'webapp/huren.html')
-	def kopen(request):
+
+def kopen(request):
 	return render(request, 'webapp/kopen.html')
-	def login(request):
+
+def login(request):
 	return render(request, 'webapp/login.html')
-	def partners(request):
+
+def partners(request):
 	return render(request, 'webapp/partners.html')
