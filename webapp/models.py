967da920dace3f33cdc0f2b3704e3ce975133ec3
Sander Bridts <bridtssander@gmail.com>
2016-04-19 12:25:17 +0200

    small fix

--- webapp/models.py
+++ webapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Panden(models.Model):
+class Pand(models.Model):
 	#id autocreated by django
 	straatnaam = models.CharField(max_length=128)
 	huisnr = models.SmallIntegerField
@@ -10,27 +10,52 @@ class Panden(models.Model):
 	handelstatus = models.ForeignKey(Handelstatus)
 	voortgang = models.ForeignKey(Voortgang)
 
+	def __str__(self):
+        return self.id
 
-class Tags(models.Model):
+
+class Tag(models.Model):
     #id autocreated by django
     tagnaam = models.CharField(max_length=128)
     Pand = models.ManyToManyField(Panden)
 
+	def __str__(self):
+        return self.id
 
-class Steden(models.Model):
+
+class Stad(models.Model):
 	postcode = models.SmallIntegerField
 	stadsnaam = models.CharField(max_length=128)
 
-class Fotos(models.Model):
+	def __str__(self):
+        return self.id
+
+class Foto(models.Model):
 	url = models.CharField(max_length=256)
 	pand = models.ManyToManyField(Panden)
+
+	def __str__(self):
+        return self.id
+
 
 class PandType(model.Model):
 	pandtype = models.CharField(max_length=128) #kantoor,huis, appartement, ...
 
+	def __str__(self):
+        return self.id
+
+
 class Handelstatus(models.Model)
 	status = models.CharField(max_length=128) #verkoop, verhuur, verpachten, ...
 
+	def __str__(self):
+        return self.id
+
+
 class Voortgang(models.Model)
 	status = models.Charfiels(max_length=128) #online, optie, bij notaris, ...
+
+	def __str__(self):
+        return self.id
+
 
