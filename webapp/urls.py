0df5d07f023c9a78abea015563b5646867444b37
Liekensjeff <liekensjeff@gmail.com>
2016-04-22 14:33:33 +0200

    urlfix

--- webapp/urls.py
+++ webapp/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     # user auth urls
     url(r'^login/$', views.login, name='login'),
     url(r'^auth/$',  views.auth_view, name='auth'),
+    url(r'^logout/$', views.logout, name='logout'),
     url(r'^loggedin/$', views.loggedin, name='loggedin'),
     url(r'^invalid/$', views.invalid_login, name='invalid'),
 
