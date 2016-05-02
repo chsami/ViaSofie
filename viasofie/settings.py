0cb2b4d345320b3d99e52b086a24b7dd907c1442
xtinne <tinne.ringo@hotmail.com>
2016-05-02 09:09:21 +0200

    Static + Login
    Static terug verplaatst + Login form afgewerkt

--- viasofie/settings.py
+++ viasofie/settings.py
@@ -82,6 +82,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'webapp/static')
 
 AUTH_USER_MODEL = 'webapp.User'
 AUTHENTICATION_BACKENDS = ['webapp.backends.EmailAuthBackend', 'django.contrib.auth.backends.ModelBackend',]
