49f2420da4c0aca2ef1757d0acaa551af8effb19
xtinne <tinne.ringo@hotmail.com>
2016-06-16 11:18:02 +0200

    /

---
+++ media/ResendEmail.py
@@ -0,0 +1,11 @@
+Bevestiging registratie
+
+
+Beste {{email}}, uw account is hierbij bevestigd. Bedankt voor uw interesse in Via Sofie.
+
+Gelieve uw account te activeren door op volgende link te klikken: {{activation_link}}
+
+
+Met vriendelijke groeten
+Sofie Andriesen - Via Sofie
+© Via Sofie. Vérdergaan in vastgoed.
