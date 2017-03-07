2f58f972e03464c27d21af44f8b817f693de3234
Sacha <sacha.vandenwyngaert@student.ap.be>
2017-03-07 23:08:55 +0100

    Fixed navbar mobile issue

--- webapp/static/webapp/scripts/index.js
+++ webapp/static/webapp/scripts/index.js
@@ -1,19 +1,3 @@
-// navigation slide-in
-$(window).load(function() {
-    $('.nav_slide_button').click(function() {
-        $('.pull').slideToggle();
-    });
-});
-
-$(function() {
-    $("#slider").responsiveSlides({
-        auto: true,
-        speed: 500,
-        namespace: "callbacks",
-        pager: true,
-    });
-});
-
 addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
 
 function hideURLbar() { window.scrollTo(0, 1); }
