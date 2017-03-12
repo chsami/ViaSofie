d0b4dfbc29913a869ff33ce5bfe85f57a305fa22
Sacha <sacha.vandenwyngaert@student.ap.be>
2017-03-12 02:52:33 +0100

    Add social share + links

--- webapp/static/webapp/scripts/pand.js
+++ webapp/static/webapp/scripts/pand.js
@@ -28,3 +28,23 @@ $(document).on('mousemove', '.activeZoom', function(event){
   $('#zoom-image').css('top', '-' + zY + 'px' );
   $('#zoom-image').css('left', '-' + zX + 'px' );
 });
+
+
+
+
+$(document).ready(function() {
+  var url = window.location;
+  var title = document.title;
+  var mnsocial = document.getElementsByClassName('mn-social-bottom');
+  mnsocial[0].href = 'https://www.facebook.com/sharer/sharer.php?u=' + url;
+  mnsocial[1].href = 'https://twitter.com/home?status=' + url + ' ' + title;
+  mnsocial[2].href = 'https://www.linkedin.com/shareArticle?mini=true&url=' + url;
+  mnsocial[3].href = 'https://plus.google.com/share?url=' + url;
+  mnsocial[4].href = 'https://pinterest.com/pin/create/link/?url=' + url;
+  mnsocial[6].href = 'http://www.tumblr.com/share/link?url=' + url;
+  mnsocial[7].href = 'https://getpocket.com/save?url=' + url;
+  mnsocial[8].href = 'http://www.stumbleupon.com/submit?url=' + url;
+  mnsocial[9].href = 'http://www.reddit.com/submit?url=' + url;
+  mnsocial[10].href = 'mailto:?&subject=' + url + '&body=' + title + ', ' + url;
+  mnsocial[11].href = 'https://delicious.com/share?url=' + url;
+});
